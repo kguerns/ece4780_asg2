8cbcc952239b74e69a544f0b99ac5dba12841857
kguerns <kguerns@clemson.edu>
2024-10-16 15:59:05 -0400

    kernel 2: dot product computed all on GPU

--- bigDot2.cu
+++ bigDot2.cu
@@ -2,19 +2,38 @@
     ECE 6780
     October 14, 2024
 
-    This program...
+    This program computes the dot product of two random single precision floating-point
+    vectors. 
+    
+    Kernel 1 computes the dot product by multiplying the vectors in-place in
+    global memory, then uses shared memory and parallel reduction to calculate partial 
+    sums for each block. Kernel 1 sends an array of partial sums back to the CPU and 
+    the CPU adds the partial sums to determine the final dot product. 
+    
+    Kernel 2 computes the dot product by multiplying the vectors in-place in global
+    memory, then uses shared memory and parallel reduction to calculate partial sums 
+    for each block. Kernel 2 uses an atomic function for read-modify-write operations
+    involved in adding up the partial sums on the GPU.
 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define N 1024*128
 #define N 16777216
+#define SHARED_MEM_AMT 1024
 
 /* Function Declarations */
 void random_floats(float *X, int vect_length);
 
+
+/*  CUDA GPU kernel 1 function to calculate partial sums of the dot product of
+    two vectors. Uses shared memory and parallel reduction. 
+    Parameters:  
+        g_A:    single precision floating point vector of length n
+        g_B:    single precision floating point vector of length n
+        g_ps1:  single precision floating point vector of length (N + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK
+        n:      length of vectors g_A and g_B                   */
 __global__ void dot_product_1(float *g_A, float *g_B, float *g_ps1, int n) {
 
     unsigned int tid = threadIdx.x;                     // thread ID
@@ -27,7 +46,7 @@ __global__ void dot_product_1(float *g_A, float *g_B, float *g_ps1, int n) {
     __syncthreads();                        // ensure all multiplication is done
 
     // Save vector segment to shared memory
-    __shared__ float smem[1024];            // allocate shared memory   // TODO: change?
+    __shared__ float smem[SHARED_MEM_AMT];          // allocate shared memory
 
     float *s_A = g_A + blockIdx.x * blockDim.x;     // find starting address of vector segment
 
@@ -49,6 +68,47 @@ __global__ void dot_product_1(float *g_A, float *g_B, float *g_ps1, int n) {
 }
 
 
+/*  CUDA GPU kernel 2 function to calculate partial sums of the dot product of
+    two vectors. Uses shared memory and parallel reduction. 
+    Parameters:  
+        g_A:    single precision floating point vector of length n
+        g_B:    single precision floating point vector of length n
+        g_dp2:  single precision floating point value
+        n:      length of vectors g_A and g_B                   */
+__global__ void dot_product_2(float *g_A, float *g_B, float *g_dp2, int n) {
+
+    unsigned int tid = threadIdx.x;                     // thread ID
+    unsigned int idx = blockIdx.x * blockDim.x + tid;   // vector index
+
+    if (idx >= n) return;                   // boundary check
+
+    // Multiply vectors in-place in global memory
+    g_A[idx] = g_A[idx] * g_B[idx];
+    __syncthreads();                        // ensure all multiplication is done
+
+    // Save vector segment to shared memory
+    __shared__ float smem[SHARED_MEM_AMT];          // allocate shared memory
+
+    float *s_A = g_A + blockIdx.x * blockDim.x;     // find starting address of vector segment
+
+    smem[tid] = s_A[tid];                   // copy vector to shared memory
+    __syncthreads();                        // ensure all copying to shared mem is done
+
+    // In-place reduction in shared memory
+    for (int stride = 1; stride < blockDim.x; stride *= 2) {
+        if ((tid % (2* stride)) == 0) {
+            smem[tid] += smem[tid + stride];
+        }
+        __syncthreads();                    // synchronize within threadblock
+    }
+
+    // Partial sum for this block is stored in thread 0
+
+    // Add partial sum result to global dot product variable
+    if (tid == 0) atomicAdd(g_dp2, smem[0]);
+}
+
+
 int main(void) {
 
     // Host Variables
@@ -56,6 +116,7 @@ int main(void) {
     int bytes = N*sizeof(float);    // size of vecotrs in bytes
     float *ps1;                     // host copy of partial sum vector
     float dp1;                      // dot product from kernel 1
+    float dp2;                      // dot product from kernel 2
 
     // Device Property Variables
     int device;                     // device number
@@ -65,7 +126,15 @@ int main(void) {
     // Device Variables
     float *d_A, *d_B;               // device copies of vectors
     float *d_ps1;                   // device copy of partial sum vector
+    float *d_dp2;                   // device copy of dot product 2
 
+    // Timers
+    cudaEvent_t start1, stop1;      // timers for kernel 1
+    cudaEventCreate(&start1);
+    cudaEventCreate(&stop1);
+    cudaEvent_t start2, stop2;      // timers for kernel 2
+    cudaEventCreate(&start2);
+    cudaEventCreate(&stop2);
 
     // Find Device Properties
     cudaGetDevice(&device);
@@ -98,27 +167,67 @@ int main(void) {
     cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice);
 
-    // TODO: Start Timer
+    // Start Timer
+    cudaDeviceSynchronize();
+    cudaEventRecord(start1);
 
     // Kernel 1
     dot_product_1<<<grid, block>>>(d_A, d_B, d_ps1, N);
-    cudaDeviceSynchronize();
 
-    // TODO:  End Timer
+    // End Timer
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop1);
 
     // Copy Partial Sums to Host
     cudaMemcpy(ps1, d_ps1, grid.x * sizeof(float), cudaMemcpyDeviceToHost);
 
-    // Add Partial Sums
+    // Add Partial Sums and Print
     dp1 = 0;
     for (int i=0; i < grid.x; i++) {
         dp1 += ps1[i];
     }
-    printf("dp1 = %f \n", dp1);
+    printf("\nKernel 1 Dot Product: %f \n", dp1);
+
+    // Timers
+    cudaEventSynchronize(stop1);
+    float k1_ms = 0;
+    cudaEventElapsedTime(&k1_ms, start1, stop1);
+
+    // Print
+    printf("Kernel 1 Time: %0.2f ms \n", k1_ms);
 
 
     // GPU Dot Product (adding atomic function or atomic lock)
 
+    // Allocate Device Memory and Copy Input Vectors to Device
+    cudaMalloc((void **) &d_dp2, sizeof(float));
+    cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice);
+    dp2 = 0;
+    cudaMemcpy(d_dp2, &dp2, sizeof(float), cudaMemcpyHostToDevice);
+
+    // Start Timer
+    cudaDeviceSynchronize();
+    cudaEventRecord(start2);
+
+    // Kernel 2
+    dot_product_2<<<grid, block>>>(d_A, d_B, d_dp2, N);
+
+    // End Timer
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop2);
+
+    // Copy Partial Sums to Host
+    cudaMemcpy(&dp2, d_dp2, sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Timers
+    cudaEventSynchronize(stop2);
+    float k2_ms = 0;
+    cudaEventElapsedTime(&k2_ms, start2, stop2);
+
+    // Prints
+    printf("\nKernel 2 Dot Product: %f \n", dp2);
+    printf("Kernel 2 Time: %0.2f ms \n", k2_ms);
 
 
     // Clean Up
@@ -128,10 +237,9 @@ int main(void) {
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_ps1);
+    cudaFree(d_dp2);
 
 }
-
-
 
 
 /* Populate a vector with floating point values
@@ -142,7 +250,6 @@ void random_floats(float *X, int vect_length) {
     //srand ( time(NULL) );
     for (int i = 0; i < vect_length; i++) {
         X[i] = (float) rand() / (float) rand();
-        //X[i] = 1;
     }
     return;
 }
